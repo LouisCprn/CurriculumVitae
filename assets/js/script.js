48bc5b837869e6c14c004d13989ab6569d1e8e6a
Louis Capron <cprnlouis@gmail.com>
2022-01-15 14:23:25 +0100

    push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
 var btnaccepted = document.querySelector('.btnaccepted')
 var btnrefuse = document.querySelector('.btnrefuse');
+var btnSettings = document.querySelector('.item');
+var btnColor = document.querySelectorAll('.btnperso')
 
 btnaccepted.addEventListener('click', function(){
     document.getElementById('cookies').style.display = "none";
-    console.log('test')
+
 })
 
 btnrefuse.addEventListener('click', function(){
     document.querySelector('.modale').style.display = "block";
-    console.log('test')
+    document.getElementById('cookies').style.display = "none";
+
+})
+
+btnSettings.addEventListener('click', function(){
+    document.querySelector('.modale').style.display = "none";
+})
+
+btnColor.addEventListener('click', function() {
+    document.querySelector('.btnAccept');
 })
