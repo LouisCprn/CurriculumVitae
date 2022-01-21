456caab5ce69afe868531a7eea2bade265cbcd4a
Louis <cprnlouis@gmail.com>
2022-01-21 10:44:12 +0100

    Push modif navbar accueil

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,12 @@
-// Les Cookies
+// NAVBAR
+function navSlide() {
+  var burger = document.querySelector('.burger');
+  var nav = document.querySelector('.nav-links');
 
-var btnaccepted = document.querySelector('.btnaccepted')
-var btnrefuse = document.querySelector('.btnrefuse');
-var btnSettings = document.querySelector('.item');
-
-btnaccepted.addEventListener('click', function () {
-  document.getElementById('cookies').style.display = "none";
-
-})
-
-btnrefuse.addEventListener('click', function () {
-  document.querySelector('.modale').style.display = "block";
-  document.getElementById('cookies').style.display = "none";
-
-})
-
-btnSettings.addEventListener('click', function () {
-  document.querySelector('.modale').style.display = "none";
-})
-
-// Section personnalisation des cookies
-
-var greenList = document.querySelectorAll('.btnperso');
-
-greenList.forEach(function (green) {
-  green.addEventListener('click', function (e) {
-    e.target.classList.toggle('btngreen')
-    e.target.nextElementSibling.classList.remove('btnred')
+  burger.addEventListener('click', function () {
+      burger.classList.toggle('show-x');
+      nav.classList.toggle('show');
   })
-})
+}
 
-var redList = document.querySelectorAll('.btnperso2');
-
-redList.forEach(function (red) {
-  red.addEventListener('click', function (e) {
-    e.target.classList.toggle('btnred')
-    e.target.previousElementSibling.classList.remove('btngreen')
-  })
-})
-
-var btnmodale = document.getElementById('btnmodale')
-
-btnmodale.addEventListener('click', function () {
-  swal("A wild Pikachu appeared! What do you want to do?", {
-    buttons: {
-      cancel: "Run away!",
-      catch: {
-        text: "Throw Pokéball!",
-        value: "catch",
-      },
-      defeat: true,
-    },
-  })
-    .then((value) => {
-      switch (value) {
-
-        case "defeat":
-          swal("Pikachu fainted! You gained 500 XP!");
-          break;
-
-        case "catch":
-          swal("Gotcha!", "Pikachu was caught!", "success");
-          break;
-
-        default:
-          swal("Got away safely!");
-      }
-    });
-})
+navSlide()
