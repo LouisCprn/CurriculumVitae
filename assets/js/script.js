413d5bbdd7e1be6b4443f6ab5e92330e5be8d19f
Louis <cprnlouis@gmail.com>
2022-01-21 14:54:12 +0100

    push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
-// NAVBAR
-function navSlide() {
-  var burger = document.querySelector('.burger');
-  var nav = document.querySelector('.nav-links');
+/* ------------------------------------ */
+/* -------------- NAVBAR -------------- */
+/* ------------------------------------ */
+var burger = document.querySelector('.burger');
+var nav = document.querySelector('.nav-links');
 
-  burger.addEventListener('click', function () {
-      burger.classList.toggle('show-x');
-      nav.classList.toggle('show');
-  })
-}
-
-navSlide()
+burger.addEventListener('click', function () {
+    burger.classList.toggle('show-x');
+    nav.classList.toggle('show');
+})
